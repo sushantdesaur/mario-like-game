2751353925956b0f9a0d924f5fcf7eea1f67a435
sushantdesaur <40424641+sushantdesaur@users.noreply.github.com>
2022-04-06 21:20:20 +0530

    Fine-tuned the game

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -185,7 +185,7 @@ function animate() {
   // Control the player movement using keyboard
   if (keys.right.pressed && player.position.x < 400) {
     player.velocity.x = player.speed;
-  } else if (keys.left.pressed && player.position.x > 100) {
+  } else if ((keys.left.pressed && player.position.x > 100) || (keys.left.pressed && scrollOffset === 0 && player.position.x > 0)) {
     player.velocity.x = -player.speed;
   } else {
     player.velocity.x = 0;
@@ -198,7 +198,7 @@ function animate() {
       genericObjects.forEach((genericObject) => {
         genericObject.position.x -= player.speed * 0.66;
       });
-    } else if (keys.left.pressed) {
+    } else if (keys.left.presse && scrollOffset > 0) {
       scrollOffset -= player.speed;
       platforms.forEach((platform) => {
         platform.position.x += player.speed;
