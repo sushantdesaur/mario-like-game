55c65759285d0c3837b6da1b447a1e344f6f4392
sushantdesaur <40424641+sushantdesaur@users.noreply.github.com>
2022-04-06 18:43:34 +0530

    Scrolled the background

--- index.js
+++ index.js
@@ -43,10 +43,10 @@ class Player {
 }
 
 class Platform {
-  constructor() {
+  constructor({x, y}) {
     this.position = {
-      x: 200,
-      y: 150,
+      x,
+      y,
     };
     this.width = 200;
     this.height = 20;
@@ -59,7 +59,12 @@ class Platform {
 
 const player = new Player();
 
-const platform = new Platform();
+// const platform = new Platform();
+const platforms = [
+  new Platform({ x: 200, y: 100 }),
+  new Platform({ x: 500, y: 200 }),
+];
+
 
 const keys = {
   right: {
@@ -74,23 +79,47 @@ function animate() {
   requestAnimationFrame(animate);
   context.clearRect(0, 0, canvas.width, canvas.height);
   player.update();
-  platform.draw();
-  if (keys.right.pressed) {
-    player.velocity.x = 5;
-  } else if (keys.left.pressed) {
-    player.velocity.x = -5;
-  } else player.velocity.x = 0;
+  platforms.forEach((platform) => {
+      platform.draw()
+  })
 
-  // Platform Collision Detection
-  if (
-    player.position.y + player.height <= platform.position.y &&
-    player.position.y + player.height + player.velocity.y >= platform.position.y &&
-    player.position.x + player.width >= platform.position.x &&
-    player.position.x < platform.position.x + platform.width
-  ) {
-    player.velocity.y = 0;
+  // Control the player movement using keyboard
+  if (keys.right.pressed && player.position.x < 400) {
+    player.velocity.x = 5;
+  } 
+  else if (keys.left.pressed && player.position.x > 100) {
+    player.velocity.x = -5;
+  } 
+  else {
+    player.velocity.x = 0;
+
+    if (keys.right.pressed) {
+        platforms.forEach((platform) => {
+          platform.position.x -= 5;
+        });
+        
+    }
+    else if (keys.left.pressed) {
+        platforms.forEach((platform) => {
+          platform.position.x += 5;
+        });
+    }
   }
-}
+ 
+  // Platform Collision Detection
+  platforms.forEach((platform) => {
+        if (
+          player.position.y + player.height <= platform.position.y &&
+          player.position.y + player.height + player.velocity.y >=
+            platform.position.y &&
+          player.position.x + player.width >= platform.position.x &&
+          player.position.x < platform.position.x + platform.width
+        ) {
+          player.velocity.y = 0;
+        }
+  })
+
+}; // End of animate function
 
 animate();
 
@@ -100,16 +129,16 @@ window.addEventListener("keydown", ({ keyCode }) => {
     case 65:
       console.log("left");
       keys.left.pressed = true;
-      player.velocity.x -= 1;
+      player.velocity.x -= 1;  //1 represents 1 step forward
       break;
     case 68:
       console.log("right");
       keys.right.pressed = true;
-      player.velocity.x += 1;
+      player.velocity.x += 1; // 1 represents 1 step forward
       break;
     case 87:
       console.log("up");
-      player.velocity.y -= 20;
+      player.velocity.y -= 20; // 20 represents 20 steps
       break;
     case 83:
       console.log("down");
